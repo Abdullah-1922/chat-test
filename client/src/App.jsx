a87a9118aa421c780cfabe5be17dae52dd8be847
Abdullah-1922 <kafikafi1922@gmail.com>
2024-12-23 10:26:39 +0000

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,73 @@
+import { Button, Container, TextField, Typography } from "@mui/material";
+import { useEffect, useMemo, useState } from "react";
+import { io } from "socket.io-client";
+const App = () => {
+  const socket = useMemo(() => {
+    return io(`http://localhost:3001`);
+  }, []);
+  const [message, setMessage] = useState("");
+  const [runningMessage, setRunningMessage] = useState([]);
+  console.log(message);
+  const handler = async (e) => {
+    e.preventDefault();
+
+    socket.emit("message", message);
+    console.log("message send");
+    setMessage("");
+    console.log(e.target.textField.value);
+
+    e.target.textField.value = "";
+  };
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected", socket.id);
+    });
+    socket.on("welcome", (s) => {
+      console.log(s);
+    });
+    socket.on("message-receive", (data) => {
+      console.log(data);
+      setRunningMessage((prev) => [...prev, data]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+  console.log(runningMessage);
+  return (
+    <Container maxWidth="sm">
+      <div>
+        <h3>Messages:</h3>
+        {runningMessage.length}
+        <div>
+          {runningMessage.map((msg, index) => {
+            return (
+              <div key={index}>
+                <h5>{msg}</h5>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <Typography variant="h1" component={"div"} gutterBottom>
+        welcome to Socket.io
+      </Typography>
+      <form onSubmit={(e) => handler(e)}>
+        <TextField
+          name="textField"
+          onChange={(e) => setMessage(e.target.value)}
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Send
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+const port = 3001;
+const app = express();
+
+const server = new createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// io is full Circuit and the socket is Individual connection.
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+  //   socket.emit("welcome",`Welcome to the server`)
+  //   socket.broadcast.emit("welcome",` ${socket.id} joined the server `)
+
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("message-receive", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User Disconnected `, socket.id);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
